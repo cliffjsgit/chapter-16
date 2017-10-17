d6dfe3e771c9131b779a259a7346d5114047c718
Philip Ulrich <philip@ulrich.life>
2017-10-17 21:54:45 +0000

    initial commit

---
+++ exercise16.1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+###############################################################################
+#
+print("\nExercise 16.1\n")
+#
+# Question 1
+# 1. Write a function called mul_time that takes a Time object and a number and
+# returns a new Time object that contains the product of the original Time and 
+# the number.
+# 
+# Then use mul_time to write a function that takes a Time object that represents
+# the finishing time in a race, and a number that represents the distance, and 
+# returns a Time object that represents the average pace (time per mile).
+#
